fde3141cba100229bb63225d010103c909e5dd14
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-07 12:16:03 +0700

    using formGroup and formControll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'cif': new FormControl(null),
+      'namaLengkap': new FormControl(null),
+      'namaIdentitas': new FormControl(null),
+      'jenisKelamin': new FormControl(null)
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.signupForm.value);
+  }
+
 
 }
